87e405aa9f4a3ae37b42cbd15a5f9cb186f46328
Richard Samuels <richard.l.samuels@gmail.com>
2024-10-27 18:46:43 -0400

    use env file

--- .github/version.cjs
+++ .github/version.cjs
@@ -1,7 +1,8 @@
 const { resolve } = require("path");
 const fs = require("node:fs");
+const p = require("process");
 
 const { version } = require(resolve("./package.json"));
 
-fs.writeFileSync(resolve(".github", "version"), version);
+fs.writeFileSync(resolve(process.env.GITHUB_OUTPUT), `version=${version}`);
 process.exit();
