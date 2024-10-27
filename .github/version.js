3490719bf8bc7e8a33af69278e6c5e3443b3da6b
Richard Samuels <richard.l.samuels@gmail.com>
2024-10-27 18:34:11 -0400

    fix again

--- .github/version.js
+++ .github/version.js
@@ -3,9 +3,4 @@ const fs = require("node:fs");
 
 const { version } = require(resolve("./package.json"));
 
-if (version) {
-  core.setOutput("version", version);
-} else {
-  core.setFailed("Unable to read version from package.json.");
-}
 fs.writeFileSync("version", version);
