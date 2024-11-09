b10a9df357a3cf71ea0380f271677aae69bc9fdb
Richard Samuels <1666322+richardsamuels@users.noreply.github.com>
2024-11-08 19:25:09 -0500

    fix(content-script): Fix message visibility on window resize (#8)

--- src/Content.svelte
+++ src/Content.svelte
@@ -7,20 +7,43 @@
     MAX_BLUR,
     MAX_INTENSITY,
   } from "./constants";
+  type Wall = {
+    // scroll Y position to start blurring
+    triggerOffset: number;
+    // The scrollY position when the Wall triggerOffset was calculated
+    scrollY: number;
+  };
   import { patternMatch } from "./Options/validator";
   import { settingsStore } from "./store.svelte";
 
   function randomItemFrom<T>(array: T[]): T {
     return array[Math.floor(Math.random() * array.length)];
   }
-
-  function scrollProgress(offset: number = 0) {
-    const amountScrolled = Math.max(window.scrollY - offset, 0);
+  function scrollProgress(threshold: number, offset: number = 0) {
+    const amountScrolled = Math.max(threshold - offset, 0);
     return Math.min(Math.floor(amountScrolled / 25), 100) / 100;
   }
 
+  const makeWall = (
+    n: number,
+    scrollY: number,
+    innerHeight: number,
+    scrollFactor: number,
+  ): Wall => {
+    let nextWall = {
+      scrollY: scrollY,
+      triggerOffset: scrollY + (n + 1) * innerHeight * scrollFactor,
+    };
+    // HACK: If we're supposed to block the whole page, force the offset to
+    // small  number immediately so that the page loads with the wall up
+    if (siteConfig.blockWholePage && n == 0) {
+      nextWall.triggerOffset = -999999;
+    }
+    return nextWall;
+  };
+
+  // Find the blacklist pattern that matches the current site
   const pattern = $derived.by(() => {
-    // Determine the pattern that caused this script to be injected
     const possiblePatterns = $settingsStore.blacklist.filter((p) =>
       patternMatch(p, window.location.toString()),
     );
@@ -32,62 +55,89 @@
       );
     }
 
-    // TODO is the most specific pattern the longest pattern?
+    // TODO MUST the most specific pattern the longest pattern?
     possiblePatterns.sort((a: string, b: string) => {
       return b.length - a.length;
     });
     return possiblePatterns[0];
   });
   const siteConfig = $derived.by(() => $settingsStore.blacklistSites[pattern]);
+  const addonEnabled = $derived($settingsStore.enabled);
 
-  const canBeVisible = $derived($settingsStore.enabled);
-
-  let numScrollExtensions = $state(1);
   let innerHeight = $state(window.innerHeight);
-  const overlayTriggerOffset = $derived.by(() => {
-    if (siteConfig.blockWholePage && numScrollExtensions == 1) {
-      return -999999;
-    }
+  let scrollY: number = $state(0);
+  let nextWall: Wall = $state(
+    makeWall(0, 0, innerHeight, siteConfig.scrollFactor),
+  );
 
-    return (
-      window.scrollY +
-      numScrollExtensions * innerHeight * siteConfig.scrollFactor
-    );
-  });
-  let rawBlurIntensity = $state(0);
+  let numScrollExtensions: number = $state(0);
+
   const blurIntensity = $derived.by(() => {
-    if (siteConfig.blockWholePage && numScrollExtensions == 1) {
-      return MAX_INTENSITY;
+    const offset = nextWall.triggerOffset;
+    if (!addonEnabled) {
+      return 0;
     }
-    return canBeVisible ? rawBlurIntensity : 0;
+
+    if (scrollY < offset) {
+      return 0;
+    }
+
+    // Calculate how far past the triggerOffset we are
+    return scrollProgress(scrollY, offset);
   });
-  const blurAmount = $derived(blurIntensity * MAX_BLUR);
-  const rgbOpacity = $derived(canBeVisible ? Math.min(blurIntensity, 0.75) : 0);
-  const pointerEvents = $derived(blurIntensity > 0.1 ? "auto" : "none");
   const messageVisible = $derived(blurIntensity > 0.1);
+  const blurAmount = $derived(blurIntensity * MAX_BLUR);
+  const rgbOpacity = $derived(addonEnabled ? Math.min(blurIntensity, 0.75) : 0);
+  const pointerEvents = $derived(blurIntensity > 0.1 ? "auto" : "none");
   let message = $state(randomItemFrom($settingsStore.messages));
+  let onNextTransition: (() => void) | null = null;
 
-  function lieToSelf(_e: Event) {
-    numScrollExtensions += 1;
-    rawBlurIntensity = 0;
-  }
-
-  $effect(() => {
-    if (!messageVisible) {
-      message = randomItemFrom($settingsStore.messages);
-    }
-  });
+  const extendScroll = (_e: Event) => {
+    // HACK: Mutate nextWall now to rerender/hide the wall, but
+    // don't increment numScrollExtensions until the animation is complete
+    // so that the user doesn't see the counter increment
+    nextWall = makeWall(
+      numScrollExtensions + 1,
+      window.scrollY,
+      innerHeight,
+      siteConfig.scrollFactor,
+    );
+    onNextTransition = () => {
+      numScrollExtensions += 1;
+    };
+  };
 
   onMount(() => {
     window.addEventListener("resize", (_) => {
       innerHeight = window.innerHeight;
+      nextWall = makeWall(
+        numScrollExtensions,
+        nextWall.scrollY,
+        innerHeight,
+        siteConfig.scrollFactor,
+      );
     });
 
     document.addEventListener("scroll", (_) => {
-      const amountScrolled = scrollProgress(overlayTriggerOffset);
-      rawBlurIntensity = amountScrolled;
+      scrollY = window.scrollY;
     });
   });
+
+  const handleAnimationEnd = () => {
+    if (onNextTransition !== null) {
+      onNextTransition();
+      onNextTransition = null;
+    }
+    if (!messageVisible) {
+      const oldMessage = $state.snapshot(message);
+      let limit = 5;
+      // Try and prevent duplicate messages
+      while (oldMessage == message && limit > 0) {
+        message = randomItemFrom($settingsStore.messages);
+        limit -= 1;
+      }
+    }
+  };
 </script>
 
 <div
@@ -96,6 +146,7 @@
   style:pointer-events={pointerEvents}
   style:backdrop-filter={`blur(${blurAmount}px)`}
   style:background-color={`rgba(0, 0, 0, ${rgbOpacity})`}
+  ontransitionend={handleAnimationEnd}
 >
   <div
     id={MESSAGE_DISPLAY_DIV_ID}
@@ -103,7 +154,10 @@
     style="gap: 84px;"
     style:opacity={messageVisible ? "1" : "0"}
   >
-    <div class="message-text-container center-flex-col" style="gap: 12px;">
+    <div
+      class="message-text-container center-flex-col soft-transition"
+      style="gap: 12px;"
+    >
       <div
         id="reason-text-div"
         class="center-text"
@@ -115,7 +169,7 @@
         {message}
       </div>
     </div>
-    <Nag n={numScrollExtensions} continueFn={lieToSelf} site={pattern} />
+    <Nag n={numScrollExtensions + 1} continueFn={extendScroll} site={pattern} />
   </div>
 </div>
 
@@ -132,7 +186,7 @@
   }
 
   .soft-transition {
-    /* transition: all 0.2s cubic-bezier(0.445, 0.05, 0.55, 0.95); */
+    transition: all 0.1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
   }
 
   .center-flex-col {
