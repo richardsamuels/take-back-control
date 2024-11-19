3c2bc7517239a991443c5248a07b88016cbe3e09
Richard Samuels <richard.l.samuels@gmail.com>
2024-11-19 09:04:34 -0500

    disable trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "off",
     headless: !!process.env.CI ? true : false,
   },
 
