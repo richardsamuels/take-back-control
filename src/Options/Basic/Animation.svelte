70ce0652ee92fea35b9773de12076d94c0d1cc0a
Richard Samuels <richard.l.samuels@gmail.com>
2024-11-20 14:45:48 -0500

    fix(options): fix visual bug with toggles

--- src/Options/Basic/Animation.svelte
+++ src/Options/Basic/Animation.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
   import { settingsStore } from "../../store.svelte";
-
-  const val = $derived($settingsStore.animation);
-
-  function click(e: Event) {
-    e.preventDefault();
-    $settingsStore.animation = !$settingsStore.animation;
-  }
 </script>
 
 <li class="list-group-item">
@@ -18,33 +11,19 @@
     </div>
     <div class="row">
       <div class="w-50">
-        <div class="btn-group btn-group-toggle" data-toggle="buttons">
-          <label
-            class="btn"
-            class:active={val}
-            class:btn-secondary={!val}
-            class:btn-success={val}
-          >
-            <input
-              type="radio"
-              autocomplete="off"
-              onclick={click}
-              checked={val}
-            /> Enabled
-          </label>
-          <label
-            class="btn btn-secondary"
-            class:active={!val}
-            class:btn-secondary={val}
-            class:btn-danger={!val}
-          >
-            <input
-              type="radio"
-              autocomplete="off"
-              onclick={click}
-              checked={!val}
-            />
-            Disabled
+        <div class="form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="enable-toggle"
+            bind:checked={$settingsStore.animation}
+          />
+          <label class="form-check-label" for="enable-toggle">
+            {#if $settingsStore.animation}
+              Animations active
+            {:else}
+              Animations disabled
+            {/if}
           </label>
         </div>
       </div>
--- src/Options/Basic/Enable.svelte
+++ src/Options/Basic/Enable.svelte
@@ -1,76 +1,56 @@
 <script lang="ts">
   import { settingsStore } from "../../store.svelte";
-
-  let val = $derived($settingsStore.enabled);
-  function click() {
-    $settingsStore.enabled = !$settingsStore.enabled;
-  }
 </script>
 
 <li class="list-group-item">
   <div class="mb-4">
     <div class="mb-4">
       <h5 class="mt-2">Plugin Status</h5>
+
+      <ul class="list-group mb-4">
+        <li
+          class="list-group-item"
+          class:list-group-item-success={$settingsStore.blacklist.length > 0}
+          class:list-group-item-danger={$settingsStore.blacklist.length == 0}
+        >
+          {#if $settingsStore.blacklist.length == 0}
+            You need to add some URLs to the blacklist
+          {:else}
+            You've got some URLs in the blacklist
+          {/if}
+        </li>
+        <li
+          class="list-group-item"
+          class:list-group-item-success={$settingsStore.messages.length > 5}
+          class:list-group-item-warning={$settingsStore.messages.length < 5}
+          class:list-group-item-danger={$settingsStore.messages.length == 0}
+        >
+          {#if $settingsStore.messages.length == 0}
+            You need to add some messages
+          {:else if $settingsStore.messages.length < 5}
+            You could add more messages, but it'll do
+          {:else}
+            You've got more than enough inspirational messages.
+          {/if}
+        </li>
+      </ul>
       <div class="mt-4">
-        <div class="btn-group btn-group-toggle" data-toggle="buttons">
-          <label
-            class="btn"
-            class:active={val}
-            class:btn-secondary={!val}
-            class:btn-success={val}
-          >
-            <input
-              type="radio"
-              autocomplete="off"
-              onclick={click}
-              checked={val}
-              data-testid="extension-enable"
-            /> Enabled
-          </label>
-          <label
-            class="btn btn-secondary"
-            class:active={!val}
-            class:btn-secondary={val}
-            class:btn-danger={!val}
-          >
-            <input
-              type="radio"
-              autocomplete="off"
-              onclick={click}
-              checked={!val}
-              data-testid="extension-disable"
-            />
-            Disabled
+        <div class="form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="enable-toggle"
+            bind:checked={$settingsStore.enabled}
+          />
+          <label class="form-check-label" for="enable-toggle">
+            {#if $settingsStore.enabled}
+              Plugin active!
+            {:else}
+              Plugin disabled
+            {/if}
           </label>
         </div>
       </div>
     </div>
-    <ul class="list-group mb-4">
-      <li
-        class="list-group-item"
-        class:list-group-item-success={$settingsStore.blacklist.length > 0}
-        class:list-group-item-danger={$settingsStore.blacklist.length == 0}
-      >
-        {#if $settingsStore.blacklist.length == 0}
-          You need to add some URLs to the blacklist
-        {:else}
-          You've got some URLs in the blacklist
-        {/if}
-      </li>
-      <li
-        class="list-group-item"
-        class:list-group-item-success={$settingsStore.messages.length > 5}
-        class:list-group-item-warning={$settingsStore.messages.length < 5}
-        class:list-group-item-danger={$settingsStore.messages.length == 0}
-      >
-        {#if $settingsStore.messages.length == 0}
-          You need to add some messages
-        {:else if $settingsStore.messages.length < 5}
-          You could add more, but it'll do
-        {:else}
-          You've got more than enough inspiration.
-        {/if}
-      </li>
-    </ul>
   </div>
 </li>
