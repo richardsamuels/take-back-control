19038ffd920b685da63649f68f7bce14dd4c1054
Richard Samuels <1666322+richardsamuels@users.noreply.github.com>
2024-11-20 14:35:35 -0500

    fix(content): prevent wall from updating message when scrolling with animation (#17)

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
   import Wall from "./Content/Wall.svelte";
-  import { onMount } from "svelte";
-  import {
-    OVERLAY_DIV_ID,
-    MESSAGE_DISPLAY_DIV_ID,
-    MAX_BLUR,
-    ONE_DAY_MINUTES,
-  } from "./constants";
   import { patternMatch } from "./Options/validator";
   import { settingsStore } from "./store.svelte";
 
-  const blacklistPattern = $derived.by(() => {
+  function findPattern(patterns: string[]) {
     // Determine the pattern that caused this script to be injected
-    const possiblePatterns = $settingsStore.blacklist.filter((p) =>
+    const possiblePatterns = patterns.filter((p) =>
       patternMatch(p, window.location.toString()),
     );
     if (!possiblePatterns) {
@@ -24,26 +17,18 @@
       return b.length - a.length;
     });
     return possiblePatterns[0];
-  });
+  }
 
-  const whitelistPattern = $derived.by(() => {
-    const possiblePatterns = $settingsStore.whitelist.filter((p) =>
-      patternMatch(p, window.location.toString()),
-    );
-    if (!possiblePatterns) {
-      return null;
-    }
-
-    // TODO: MUST the most specific pattern the longest pattern?
-    possiblePatterns.sort((a: string, b: string) => {
-      return b.length - a.length;
-    });
-    return possiblePatterns[0];
-  });
+  const blacklistPattern = $derived.by(() =>
+    findPattern($settingsStore.blacklist),
+  );
+  const whitelistPattern = $derived.by(() =>
+    findPattern($settingsStore.whitelist),
+  );
 </script>
 
 <div>
   {#if blacklistPattern != null && whitelistPattern == null}
-    <Wall {blacklistPattern} {whitelistPattern} />
+    <Wall {blacklistPattern} />
   {/if}
 </div>
