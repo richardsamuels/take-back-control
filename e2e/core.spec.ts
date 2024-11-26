27be0fa82e8d0b51f86a0731b05879678acd9805
Richard Samuels <richard.l.samuels@gmail.com>
2024-11-25 22:52:55 -0500

    fix timeout

--- e2e/core.spec.ts
+++ e2e/core.spec.ts
@@ -36,7 +36,7 @@ const __dirname = path.dirname(__filename);
 
 test("blacklist test", async ({ page, extensionId }) => {
   // This test takes at least 10 seconds, but in CI its much worse
-  test.setTimeout(!!env.CI ? 60_000 : 20_000);
+  test.setTimeout(!!env.CI ? 120_000 : 20_000);
   await setup(page, extensionId);
 
   await page.goto(`chrome-extension://${extensionId}/src/options.html#/`);
