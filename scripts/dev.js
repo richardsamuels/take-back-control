f926148442976f05b4c90c6b4fca1d2db66959b6
Richard Samuels <1666322+richardsamuels@users.noreply.github.com>
2024-12-02 17:17:53 -0500

    fix(options): removes extra history state from router (#32)

---
+++ scripts/dev.js
@@ -0,0 +1,7 @@
+console.log(
+  "!!! `yarn dev` has been disabled. This build does not support HMR,",
+);
+console.log(
+  "so you should use `yarn watch` and install the extension via your",
+);
+console.log("browser's dev tools");
